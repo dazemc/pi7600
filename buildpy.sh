f54d99feb0579c07b1bec089a9f965f7eca59f82
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-03-09 01:21:49 -0800

    skip confirmation on package install

--- buildpy.sh
+++ buildpy.sh
@@ -13,7 +13,7 @@ dphys-swapfile setup
 dphys-swapfile swapon
 echo "Updating and installing dependencies"
 apt-get update && sudo apt-get upgrade -y
-apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
+apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
 echo "Downloading Python 3.12.2"
 wget https://www.python.org/ftp/python/3.12.2/Python-3.12.2.tgz
 echo "Extracting.."
