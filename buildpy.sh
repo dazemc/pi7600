4b09a5747591ce4eefa2751659d7561bfa3326a8
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-03-08 23:38:14 -0800

    Stop clearing screen, hard to debug

--- buildpy.sh
+++ buildpy.sh
@@ -5,7 +5,6 @@ sudo rm /etc/dphys-swapfile
 echo "CONF_SWAPSIZE=2048" | sudo tee /etc/dphys-swapfile
 sudo dphys-swapfile setup
 sudo dphys-swapfile swapon
-clear
 echo "Updating and installing dependencies"
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
@@ -14,10 +13,9 @@ wget https://www.python.org/ftp/python/3.13.0/Python-3.13.0a4.tgz
 echo "Extracting.."
 tar xvf Python-3.13.0a4.tgz
 cd Python-3.13.0a4 || exit
-clear
 echo "Building, this will take a while..."
 ./configure --enable-optimizations --with-ensurepip=install
-make -j$(nproc)
+make -j 'nproc'
 make altinstall
 echo "Resetting swap"
 sudo dphys-swapfile swapoff
