bb84e587f9287dbef9804dae0b8911de30101f83
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-03-09 00:47:01 -0800

    switch to latest stable release

--- buildpy.sh
+++ buildpy.sh
@@ -15,9 +15,9 @@ echo "Updating and installing dependencies"
 apt-get update && sudo apt-get upgrade -y
 apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
 echo "Downloading Python 3.13"
-wget https://www.python.org/ftp/python/3.13.0/Python-3.13.0a4.tgz
+wget https://www.python.org/ftp/python/3.12.2/Python-3.12.2.tgz
 echo "Extracting.."
-tar xvf Python-3.13.0a4.tgz
+tar xvf Python-3.12.2.tgz
 cd Python-3.13.0a4 || exit
 echo "Building, this will take a while..."
 ./configure --enable-optimizations --with-ensurepip=install
