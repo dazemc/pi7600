a1c61e17cebb422bc28526ea2f3a9777b07e80a3
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-03-09 02:00:11 -0800

    automatically download latest stable source

--- buildpy.sh
+++ buildpy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+  GNU nano 6.2                                                                                                               test.sh                                                                                                                        #!/bin/bash
 echo "Checking for root"
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
@@ -15,13 +15,28 @@ echo "Updating and installing dependencies"
 apt-get update && sudo apt-get upgrade -y
 apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
 echo "Downloading Python 3.12.2"
-wget https://www.python.org/ftp/python/3.12.2/Python-3.12.2.tgz
+url='https://www.python.org/ftp/python/'
+
+curl --silent "$url" |
+    sed -n 's!.*href="\([0-9]\+\.[0-9]\+\.[0-9]\+\)/".*!\1!p' |
+    sort -rV |
+{
+while read -r version; do
+    filename="Python-$version"
+    # Versions which only have alpha, beta, or rc releases will fail here.
+    # Stop when we find one with a final release.
+    if curl --fail --silent -O "$url/$version/$filename.tgz"; then
+        echo "$filename"
+        break
+    fi
+done
 echo "Extracting.."
-tar xvf Python-3.12.2.tgz
-cd Python-3.12.2 || exit
+tar xvf "$filename.tgz"
+cd Python-"$version" || exit
+}
 echo "Building, this will take a while..."
 ./configure --enable-optimizations --with-ensurepip=install
-make -j $(nproc)
+make -j "$(nproc)"
 make altinstall
 echo "Resetting swap"
 dphys-swapfile swapoff
