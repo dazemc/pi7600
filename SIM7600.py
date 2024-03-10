de32154a9fdc80213d9bd1f1457eb8d2c1c9f861
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-03-09 18:58:24 -0800

    output buffer

--- SIM7600.py
+++ SIM7600.py
@@ -2,5 +2,5 @@ from SMS import SMS
 
 contact_number = "******"
 messaging = SMS(contact_number)
-received_buffer = messaging.read_message(message_type="ALL")
-print(received_buffer)
+messaging.read_message(message_type="ALL")
+
