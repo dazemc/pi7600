caaf8a0c8ac5afb6865ff869e61e9040d2a4f27e
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-03-12 23:18:19 -0700

    TODO: Separate Phone.call() into multiple functions

--- Phone.py
+++ Phone.py
@@ -15,17 +15,28 @@ class Phone(Settings):
         super().__init__()
 
     def call(self, contact_number: str) -> bool:
+        # TODO: Break into functions
+        # init_checks(): Check for signal, network registration, open/incoming calls, etc
+        # call_incoming(): Check for incoming calls
+        # call(): Make a call
+        # hangup_call(): Hangup a call
+        # open_call(): Function executes during call, checking if call is still open
+        # close_call(): Function executes after successful call
         try:
             while True:
                 print(f"Attempting to call {contact_number}")
-                self.send_at('AT+CSQ', 'OK', PHONE_TIMEOUT)
-                self.send_at('AT+CREG?', 'OK', PHONE_TIMEOUT)
-                self.send_at('AT+CPSI?', 'OK', PHONE_TIMEOUT)
+                #
+                #
+                self.send_at('AT+CSQ', 'OK', PHONE_TIMEOUT)  # Check network quality
+                self.send_at('AT+CREG?', 'OK', PHONE_TIMEOUT)  # Check network registration
+                self.send_at('AT+CPSI?', 'OK', PHONE_TIMEOUT)  # Pretty much returns the previous two commands... Unnecessarily redundant?
+                #
+                #
                 self.send_at('ATD' + contact_number + ';', 'OK', PHONE_TIMEOUT)
-                time.sleep(20)
-                self.ser.write('AT+CHUP\r\n'.encode())
+                time.sleep(20)  # Call just closes on a timer.
+                self.ser.write('AT+CHUP\r\n'.encode())  # Hangup code, why is serial used vs send_at()?
                 print('Call disconnected')
         except:
             return False
-        finally:
+        finally:  # TODO: Proper return for current call
             return True
