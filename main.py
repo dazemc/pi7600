cb2e01d15cf95a618775b80f8c5e3550852148f8
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-09-17 20:59:32 -0700

    documentation/typing

--- main.py
+++ main.py
@@ -15,50 +15,19 @@ from AT import AT
 app = FastAPI()
 cwd = os.getcwd()
 sms = SMS()
-com_watch = AT(com=WATCHER_COM, baudrate=BAUDRATE)  # Might separate this into systemd, just polls serial and reacts.
-
-
-# SETTINGS for persistent modem configs
-# settings = Settings()
-# settings.set_data_mode(1)
-# print(settings.get_config)
-# settings.enable_verbose_logging()  # Only needs to be enabled once
-# settings.set_sms_storage("SM")
-
-
-# GPS
-# gps = GPS()
-# gps_cor = gps.get_gps_position()
-# print(gps_cor)
-
-# SMS
-# contact_number = "+11234567890"  # Number you are sending to, +CC (Country Code)
-# message = "Hey"
-# buffer = messaging.read_message(message_type="ALL")
-# print("Sending messages...")
-# print(buffer)
-
-# Execute script from most recent text, can iterate through all and look for header('pw')
-# TODO: TOTP
-# message = buffer[-1]["message_contents"]
-# if message[:2] == "pw":
-#     if message[2:8] == '123456':  # can be encrypted just keep 559 char limit
-#         script = message[8:]  # TODO: add method to execute based off file type
-#         subprocess.call(f"{cwd}/scripts/{script}")  # don't forget to chmod +x
-# Send message, returns True on success
-# messaging.send_message(contact_number, message)
-
-# PHONE
-# phone = Phone()
-# phone.call(contact_number)
-# phone.close_serial()
+com_watch = AT(
+    com=WATCHER_COM, baudrate=BAUDRATE
+)  # Might separate this into systemd, just polls serial and reacts.
 
 
 # API
-
-
 @app.get("/info", status_code=status.HTTP_200_OK)
-async def root():
+async def root() -> dict:
+    """Device Information
+
+    Returns:
+        dict: hostname, uname, date, arch
+    """
     hostname = subprocess.run(
         ["hostname"], capture_output=True, text=True, check=False
     ).stdout.strip()
@@ -80,27 +49,89 @@ async def root():
 
 
 @app.get("/sms", status_code=status.HTTP_200_OK)
-async def sms_root(msg_query: str = "ALL"):
-    # Read message lists, by message type ("ALL", "REC READ", "REC UNREAD", "STO UNSENT", "STO SENT")
+async def sms_root(msg_query: str = "ALL") -> dict | None:
+    """Read messages from modem
+
+
+    Args:
+        msg_query (str, optional): ["ALL", "REC READ", "REC UNREAD", "STO UNSENT", "STO SENT"]. Defaults to "ALL".
+
+    Returns:
+        dict: {"response": Messages} | {"response": "null"}
+    """
     resp = sms.read_message(message_type=msg_query)
     return resp
 
 
 @app.get("/sms/delete/{msg_idx}", status_code=status.HTTP_202_ACCEPTED)
-async def delete_msg(msg_idx: int):
+async def delete_msg(msg_idx: int) -> dict:
+    """Delete sms message by MODEM index
+
+    Args:
+        msg_idx (int): MODEM message index
+
+    Returns:
+        dict: {"response": "Success"} | False
+    """
     resp = sms.delete_message(msg_idx)
     return resp
 
 
 @app.post("/sms", status_code=status.HTTP_201_CREATED)
-async def send_msg(msg: str, number: str):
+async def send_msg(msg: str, number: str) -> dict:
+    """POST SMS Message to destination number
+
+    Args:
+        msg (str): sms text body
+        number (str): sms destination number
+
+    Returns:
+        dict: {"response": True}
+    """
     resp = sms.send_message(phone_number=number, text_message=msg)
-    return resp
+    return {"response": resp}
 
 
 @app.post("/at", status_code=status.HTTP_202_ACCEPTED)
-async def catcmd(cmd: str = "AT"):
+async def catcmd(cmd: str = "AT") -> str:
+    """Sends raw AT commands to modem, will not work with commands that require input, return response
+
+    Args:
+        cmd (str, optional): Defaults to "AT".
+
+    Returns:
+        str: raw stdout response if "OK" or "ERROR" if "\r\n" is returned
+    """
     resp = subprocess.run(
         ["./scripts/catcmd", cmd], capture_output=True, text=True, check=False
     ).stdout
     return resp
+
+
+# SETTINGS for persistent modem configs
+# settings = Settings()
+# settings.set_data_mode(1)
+# print(settings.get_config)
+# settings.enable_verbose_logging()  # Only needs to be enabled once
+# settings.set_sms_storage("SM")
+
+
+# GPS
+# gps = GPS()
+# gps_cor = gps.get_gps_position()
+# print(gps_cor)
+
+# Execute script from most recent text, can iterate through all and look for header('pw')
+# TODO: TOTP
+# message = buffer[-1]["message_contents"]
+# if message[:2] == "pw":
+#     if message[2:8] == '123456':  # can be encrypted just keep 559 char limit
+#         script = message[8:]  # TODO: add method to execute based off file type
+#         subprocess.call(f"{cwd}/scripts/{script}")  # don't forget to chmod +x
+# Send message, returns True on success
+# messaging.send_message(contact_number, message)
+
+# PHONE
+# phone = Phone()
+# phone.call(contact_number)
+# phone.close_serial()
