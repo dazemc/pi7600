75a108f74f29df1cb2d3a2317ad842eb47ed080d
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-09-17 21:31:40 -0700

    optimize parser

--- Parser.py
+++ Parser.py
@@ -1,11 +1,16 @@
 def get_message_type(message: str) -> str:
+    message = message.rstrip().replace("\x00", "")
+
     try:
+        # Try to interpret message as hexadecimal
         int(message, 16)
         message_b = bytes.fromhex(message)
         message = message_b.decode("utf-8")
-        return message.rstrip().replace("\x00", "")
-    except:
-        return message.rstrip().replace("\x00", "")
+    except ValueError:
+        # Return original message if conversion fails
+        pass
+
+    return message
 
 
 class Parser:
@@ -14,37 +19,34 @@ class Parser:
 
     def parse_sms(self, sms_buffer: str) -> list:
         """
-        Parses the modem buffer into list of dictionaries
+        Parses the modem sms buffer into a list of dictionaries
         :param sms_buffer: str
         :return: list<dict>
         """
-        read_messages = []
-        for line in sms_buffer.split("\r\n"):
-            read_messages.append(line)
+        read_messages = sms_buffer.split("\r\n")
         message_list = []
-        message_data = [
-            message.split(",")
-            for i, message in enumerate(read_messages)
-            if i % 2 != 0 and len(message) > 1
-        ]
-        message_text = [
-            message for i, message in enumerate(read_messages) if i % 2 == 0 and i != 0
-        ]
-        for i, message in enumerate(message_data):
-            message_list.append(
-                {
-                    # idx 0 is oldest message, this is relative to at+cmgd/cmgr
-                    "message_index": message[0][message[0].rfind(" ") + 1 :],
-                    "message_type": get_message_type(message[1].replace('"', "")),
-                    "message_originating_address": get_message_type(
-                        message[2].replace('"', "")
-                    ),
-                    "message_destination_address": get_message_type(
-                        message[3].replace('"', "")
-                    ),
-                    "message_date": message[4][1:],
-                    "message_time": message[5][:-1],
-                    "message_contents": get_message_type(message_text[i]),
-                }
-            )
+
+        for i in range(1, len(read_messages), 2):
+            if len(read_messages[i]) > 1:
+                message = read_messages[i].split(",")
+                if len(message) >= 6:  # Ensure we have enough fields
+                    message_list.append(
+                        {
+                            "message_index": message[0][message[0].rfind(" ") + 1 :],
+                            "message_type": get_message_type(
+                                message[1].replace('"', "")
+                            ),
+                            "message_originating_address": get_message_type(
+                                message[2].replace('"', "")
+                            ),
+                            "message_destination_address": get_message_type(
+                                message[3].replace('"', "")
+                            ),
+                            "message_date": message[4][1:],  # Remove starting quote
+                            "message_time": message[5][:-1],  # Remove trailing quote
+                            "message_contents": get_message_type(
+                                read_messages[i - 1]
+                            ),  # Even index for message text
+                        }
+                    )
         return message_list
