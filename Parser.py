26d18653baa694474d21806e8104d0d2b68169b1
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-09-17 21:58:21 -0700

    update parser

--- Parser.py
+++ Parser.py
@@ -9,30 +9,30 @@ class Parser:
         :return: list<dict>
         """
         read_messages = sms_buffer.split("\r\n")
+        read_messages = read_messages[1:-3]
         message_list = []
 
 
         for i in read_messages:
-            if len(i) > 1:
-                message = i.split(",")
-                print(message)
-                message_list.append(
-                    {
-                        "message_index": message[0][message[0].rfind(" ") + 1 :],
-                        "message_type":
-                            message[1].replace('"', "")
-                        ,
-                        "message_originating_address":
-                            message[2].replace('"', "")
-                        ,
-                        "message_destination_address":
-                            message[3].replace('"', "")
-                        ,
-                        "message_date": message[4][1:],
-                        "message_time": message[5][:-1],
-                        "message_contents":
-                            read_messages[i - 1]
-                        ,
-                    }
-                )
-        return message_list
+            message = i.split(",")
+            print(message)
+            message_list.append(
+                {
+                    "message_index": message[0][message[0].rfind(" ") + 1 :],
+                    "message_type":
+                        message[1].replace('"', "")
+                    ,
+                    "message_originating_address":
+                        message[2].replace('"', "")
+                    ,
+                    "message_destination_address":
+                        message[3].replace('"', "")
+                    ,
+                    "message_date": message[4][1:],
+                    "message_time": message[5][:-1],
+                    "message_contents":
+                        read_messages[i - 1]
+                    ,
+                }
+            )
+    return message_list
