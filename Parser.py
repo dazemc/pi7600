1bb1481a6d713bda2894f64a5adc707cbee49c8f
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-09-17 22:07:56 -0700

    all odd idxs are message conents for the previous idx

--- Parser.py
+++ Parser.py
@@ -10,29 +10,31 @@ class Parser:
         """
         read_messages = sms_buffer.split("\r\n")
         read_messages = read_messages[1:-3]
+        print(read_messages)
         message_list = []
 
 
         for i, v in enumerate(read_messages):
-            message = v.split(",")
-            print(message)
-            message_list.append(
-                {
-                    "message_index": message[0][message[0].rfind(" ") + 1 :],
-                    "message_type":
-                        message[1].replace('"', "")
-                    ,
-                    "message_originating_address":
-                        message[2].replace('"', "")
-                    ,
-                    "message_destination_address":
-                        message[3].replace('"', "")
-                    ,
-                    "message_date": message[4][1:],
-                    "message_time": message[5][:-1],
-                    "message_contents":
-                        read_messages[i + 1]
-                    ,
-                }
-            )
-            return message_list
+            if i % 2 == 0:
+                message = v.split(",")
+                print(message)
+                message_list.append(
+                    {
+                        "message_index": message[0][message[0].rfind(" ") + 1 :],
+                        "message_type":
+                            message[1].replace('"', "")
+                        ,
+                        "message_originating_address":
+                            message[2].replace('"', "")
+                        ,
+                        "message_destination_address":
+                            message[3].replace('"', "")
+                        ,
+                        "message_date": message[4][1:],
+                        "message_time": message[5][:-1],
+                        "message_contents":
+                            read_messages[i + 1]
+                        ,
+                    }
+                )
+                return message_list
