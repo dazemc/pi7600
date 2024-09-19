76b2e0eb2578c33e398b80f07d13c30aeec3aebe
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-09-19 04:04:20 -0700

    revert log config name change

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uvicorn main:app --host 0.0.0.0 --log-config .log.yaml --reload --reload-exclude __pycache__
+uvicorn main:app --host 0.0.0.0 --log-config log.yaml --reload --reload-exclude __pycache__
