575c7b04c9f01e9517e39a60ef2d6d8884ce71d9
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-09-19 06:19:55 -0700

    bash syntax

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ LOG_PATH="/var/log/pi7600/uvicorn.log"
 if [ -f "$LOG_PATH" ]; then
     mv "$LOG_PATH" "${LOG_PATH}_old"
 fi
-touch "{$LOG_PATH}"
+touch "$LOG_PATH"
 uvicorn main:app --host 0.0.0.0 --log-config log.yaml --reload
