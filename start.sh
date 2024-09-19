e3035777adff1c8df9611c8de9ea3b09e294c280
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-09-19 06:13:04 -0700

    change repo name, add dir for logs, backup log and create empty one

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+LOG_PATH="/var/log/pi7600/uvicorn.log"
+if [ -f "$LOG_PATH" ]; then
+    mv "$LOG_PATH" "${LOG_PATH}_old"
+fi
+touch "{$LOG_PATH}"
 uvicorn main:app --host 0.0.0.0 --log-config log.yaml --reload
